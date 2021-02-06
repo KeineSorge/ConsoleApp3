001f3aead8f4b90a27164afd6c7e9a21b740cc34
Nihat <serimnihat@gmail.com>
2021-02-06 12:56:54 +0300

    Add project files.

---
+++ ConsoleApp3/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class MyDictionary<TCode, TCity>
+    {
+        TCode[] codes;
+        TCity[] cities;
+
+        public MyDictionary()
+        {
+            codes = new TCode[0];
+            cities = new TCity[0];
+        }
+
+        public void Add(TCode code, TCity city)
+        {
+            TCode[] tempCode = codes;
+            TCity[] tempCity = cities;
+
+            codes = new TCode[codes.Length + 1];
+            cities = new TCity[cities.Length + 1];
+
+            for (int i = 0; i < tempCode.Length; i++)
+            {
+                codes[i] = tempCode[i];
+            }
+
+            codes[codes.Length - 1] = code;
+
+            for (int i = 0; i < tempCity.Length; i++)
+            {
+                cities[i] = tempCity[i];
+            }
+
+            cities[cities.Length - 1] = city;
+
+            Console.WriteLine("Plaka Kodu : {0} --- Şehir Adı : {1}", code, city);
+        }
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(01, "Adana");
+            myDictionary.Add(02, "Adıyaman");
+            myDictionary.Add(03, "Afyon");
+        }
+    }
+}
